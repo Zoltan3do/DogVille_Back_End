77a7726689e9779cbf4a41db844cd8e4377a4aa2
Manuel Barone <baronemanuel05@gmail.com>
2024-12-05 18:27:19 +0100

    cambiamenti jwt

--- DogVilleBE/src/main/java/manu_barone/DogVille/entities/enums/Taglia.java
+++ DogVilleBE/src/main/java/manu_barone/DogVille/entities/enums/Taglia.java
@@ -1,5 +1,5 @@
 package manu_barone.DogVille.entities.enums;
 
 public enum Taglia {
-    NANA,PICCOLA, MEDIA,GRANDE,GIGANTE
+    NANA,PICCOLA,MEDIA,GRANDE,GIGANTE
 }
--- DogVilleBE/src/main/java/manu_barone/DogVille/payloads/UtenteDTO.java
+++ DogVilleBE/src/main/java/manu_barone/DogVille/payloads/UtenteDTO.java
@@ -5,26 +5,27 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import manu_barone.DogVille.payloads.validationGroups.Create;
+import manu_barone.DogVille.payloads.validationGroups.Update;
 
 public record UtenteDTO(
         @NotEmpty(message = "Il nome è obbligatorio!" , groups = Create.class)
-        @Size(min = 2, max = 40, message = "Il nome deve essere compreso tra 2 e 40 caratteri!")
+        @Size(min = 2, max = 40, message = "Il nome deve essere compreso tra 2 e 40 caratteri!",groups = {Create.class, Update.class})
         String name,
         @NotEmpty(message = "Il cognome è obbligatorio!", groups = Create.class)
-        @Size(min = 2, max = 40, message = "Il cognome deve essere compreso tra 2 e 40 caratteri!")
+        @Size(min = 2, max = 40, message = "Il cognome deve essere compreso tra 2 e 40 caratteri!",groups = {Create.class, Update.class})
         String surname,
         @NotEmpty(message = "Lo username è obbligatorio!", groups = Create.class)
-        @Email(message = "L'email inserita non è un'email valida!")
-        @Size(min = 4, message = "La email deve avere almeno 4 caratteri!")
+        @Email(message = "L'email inserita non è un'email valida!",groups = {Create.class, Update.class})
+        @Size(min = 4, message = "La email deve avere almeno 4 caratteri!",groups = {Create.class, Update.class})
         String email,
         @NotEmpty(message = "password vuota", groups = Create.class)
-        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "La password non segue i criteri comuni")
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "La password non segue i criteri comuni" , groups = {Create.class, Update.class})
         String password,
         @NotEmpty(message = "L'indirizzo è obbligatorio!", groups = Create.class)
-        @Size(min = 4, message = "L'indirizzo deve avere almeno 4 caratteri!")
+        @Size(min = 4, message = "L'indirizzo deve avere almeno 4 caratteri!",groups = {Create.class, Update.class})
         String address,
         @NotEmpty(groups = Create.class)
-        @Pattern(regexp = "^\\+39\\d{10}$")
+        @Pattern(regexp = "^\\+39\\d{10}$", groups = {Create.class, Update.class})
         String telephoneNumber
 ) {
 }
